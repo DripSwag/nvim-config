9bffcbe92f903154411ab9fe75c139e7aaec018b
DripSwag <mimirani33@gmail.com>
2024-09-12 20:04:45 +0100

    Removed inlay hint

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -7,5 +7,11 @@ if true then
     { "echasnovski/mini.ai", enabled = false },
     { "RRethy/vim-illuminate", enabled = false },
     { "lukas-reineke/indent-blankline.nvim", enabled = false },
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        inlay_hints = { enabled = false },
+      },
+    },
   }
 end
